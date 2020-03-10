6e8988e870571e2e9cd493d22fb9854a2737e904
dnaumov <dnaumov@acumatica.com>
2020-03-10 11:41:22 -0400

    Improve support for SOAP Like code

--- SOAPLikeWrapperForREST/SOAPTypeWrappers/SOAPStringTypesWrappers.cs
+++ SOAPLikeWrapperForREST/SOAPTypeWrappers/SOAPStringTypesWrappers.cs
@@ -7,6 +7,96 @@ using System.Threading.Tasks;
 
 namespace SOAPLikeWrapperForREST
 {
+    public enum GuidCondition
+    {
+
+        /// <remarks/>
+        Equal,
+
+        /// <remarks/>
+        NotEqual,
+
+        /// <remarks/>
+        IsNull,
+
+        /// <remarks/>
+        IsNotNull,
+    }
+    public enum IntCondition
+    {
+
+        /// <remarks/>
+        Equal,
+
+        /// <remarks/>
+        NotEqual,
+
+        /// <remarks/>
+        IsBetween,
+
+        /// <remarks/>
+        IsGreaterThan,
+
+        /// <remarks/>
+        IsLessThan,
+
+        /// <remarks/>
+        IsGreaterThanOrEqualsTo,
+
+        /// <remarks/>
+        IsLessThanOrEqualsTo,
+
+        /// <remarks/>
+        IsNull,
+
+        /// <remarks/>
+        IsNotNull,
+    }
+    public enum DateTimeCondition
+    {
+
+        /// <remarks/>
+        Equal,
+
+        /// <remarks/>
+        NotEqual,
+
+        /// <remarks/>
+        IsBetween,
+
+        /// <remarks/>
+        IsGreaterThan,
+
+        /// <remarks/>
+        IsLessThan,
+
+        /// <remarks/>
+        IsGreaterThanOrEqualsTo,
+
+        /// <remarks/>
+        IsLessThanOrEqualsTo,
+
+        /// <remarks/>
+        Today,
+
+        /// <remarks/>
+        Overdue,
+
+        /// <remarks/>
+        Tomorrow,
+
+        /// <remarks/>
+        ThisWeek,
+
+        /// <remarks/>
+        ThisMonth,
+
+        /// <remarks/>
+        IsNull,
+
+        /// <remarks/>
+        IsNotNull,
+    }
     public enum StringCondition
     {
         /// <remarks/>
@@ -47,6 +137,69 @@ namespace SOAPLikeWrapperForREST
 
         /// <remarks/>
         IsNotNull,
+    }
+    public partial class GuidSearch : GuidValue
+    {
+
+        private GuidCondition conditionField;
+
+        private System.Guid value2Field;
+
+        public GuidCondition Condition
+        {
+            get
+            {
+                return this.conditionField;
+            }
+            set
+            {
+                this.conditionField = value;
+            }
+        }
+
+        public System.Guid Value2
+        {
+            get
+            {
+                return this.value2Field;
+            }
+            set
+            {
+                this.value2Field = value;
+            }
+        }
+    }
+    public partial class IntSearch : IntValue
+    {
+
+        private IntCondition conditionField;
+
+        private System.Nullable<int> value2Field;
+
+
+        public IntCondition Condition
+        {
+            get
+            {
+                return this.conditionField;
+            }
+            set
+            {
+                this.conditionField = value;
+            }
+        }
+
+        public System.Nullable<int> Value2
+        {
+            get
+            {
+                return this.value2Field;
+            }
+            set
+            {
+                this.value2Field = value;
+            }
+        }
     }
     public partial class StringSearch : StringValue
     {
