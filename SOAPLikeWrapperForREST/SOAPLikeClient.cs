5cda516843651462b7796ef88bf83f49c0f63db7
dnaumov <dnaumov@acumatica.com>
2020-07-22 15:02:57 -0400

    Improve SOAP wrapper (add more methods, refactor the code, etc.)

--- SOAPLikeWrapperForREST/SOAPLikeClient.cs
+++ SOAPLikeWrapperForREST/SOAPLikeClient.cs
@@ -40,6 +40,20 @@ namespace SOAPLikeWrapperForREST
         public void Logout()
         {
             AuthorizationApi.AuthLogout();
+            BusinessDate = null;
+        }
+
+        public T GetById<T>(Guid? id, string select = null, string filter = null, string expand = null, string custom = null)
+            where T : Entity
+        {
+            SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
+            return api.GetById(id, select, filter, expand, custom);
+        }
+        public T GetByKeys<T>(IEnumerable<string> ids, string select = null, string filter = null, string expand = null, string custom = null)
+            where T : Entity
+        {
+            SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
+            return api.GetByKeys(ids, select, filter, expand, custom);
         }
 
         [Obsolete("Get method is for backward compatibility with SOAP only. Use one of the following REST methods instead: GetList, GetByKeys, GetByID")]
@@ -50,19 +64,26 @@ namespace SOAPLikeWrapperForREST
             SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
             if (entity.ID.HasValue)
             {
-                return api.GetById(entity.ID, expand: expand);
+                T resultByID = api.GetById(entity.ID, expand: expand);
+                resultByID.ReturnBehavior = entity.ReturnBehavior;
+                return resultByID;
             }
             string filter = ComposeFilter(entity);
-            var result = api.GetList(filter: filter);
-            if (result.Count > 1)
+            var list = api.GetList(filter: filter);
+            if (list.Count > 1)
             {
                 throw new Exception("More than one entity satisfies the condition.");
             }
-
-            return api.GetById(result.FirstOrDefault().ID, expand: expand);
+            if (list.Count == 0)
+            {
+                throw new Exception("No entities satisfy the condition.");
+            }
+            T result = api.GetById(list[0].ID, expand: expand);
+            result.ReturnBehavior = entity.ReturnBehavior;
+            return result;
         }
         public T[] GetList<T>(T entity, int? top = null, int? skip = null)
- where T : Entity
+            where T : Entity
         {
             string expand = ComposeExpands(entity);
             SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
@@ -76,7 +97,9 @@ namespace SOAPLikeWrapperForREST
             where T : Entity
         {
             SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
-            return api.PutEntity(entity, expand: ComposeExpands(entity), businessDate: BusinessDate);
+            var result = api.PutEntity(entity, expand: ComposeExpands(entity), businessDate: BusinessDate);
+            result.ReturnBehavior = entity.ReturnBehavior;
+            return result;
         }
         [Obsolete("PutFiles method is for backward compatibility with SOAP only. Use one of the following REST methods instead: PutFile, PutFileAsync")]
         public void PutFiles<T>(List<string> keys, File[] files)
@@ -89,20 +112,45 @@ namespace SOAPLikeWrapperForREST
             SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
             api.PutFile(keys, files[0].Name, files[0].Content);
         }
+        [Obsolete("GetFiles method is for backward compatibility with SOAP only. Use one of the following REST methods instead: PutFile, PutFileAsync")]
+        public File[] GetFiles<T>(T entity)
+            where T : Entity
+        {
+            SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
+            return new File[] { };
+        }
 
         [Obsolete("Delete method is for backward compatibility with SOAP only. Use one of the following REST methods instead: DeleteByID, DeleteByKeys")]
         public void Delete<T>(T entity)
             where T : Entity
         {
+            if (entity.ID != null) DeleteById<T>(entity.ID);
+            else DeleteById<T>(Get(entity).ID);
+        }
+
+        /// <summary>
+        /// Deletes the record by its session identifier. 
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when fails to make API call</exception>
+        /// <param name="id">The session ID of the record.</param>
+        /// <returns></returns>
+        public void DeleteById<T>(Guid? id) where T : Entity
+        {
             SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
-            if (entity.ID != null)
-            {
-                api.DeleteById(entity.ID);
-            }
-            else
-            {
-                api.DeleteById(Get(entity).ID);
-            }
+            api.DeleteById(id);
+        }
+
+        /// <summary>
+        /// Deletes the record by its keys. 
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when fails to make API call</exception>
+        /// <param name="id">The session ID of the record.</param>
+        /// <returns></returns>
+        public void DeleteByKeys<T>(IEnumerable<string> ids)
+            where T : Entity
+        {
+            SOAPLikeEntityAPI<T> api = new SOAPLikeEntityAPI<T>(CurrentConfiguration);
+            api.DeleteByKeys(ids);
         }
 
         public string Invoke<T>(EntityAction<T> action)
@@ -141,8 +189,8 @@ namespace SOAPLikeWrapperForREST
             action.Entity = entity;
             return WaitInvoke(action);
         }
-        public ProcessResult WaitInvoke<T>(EntityAction<T> action)
-        where T : Entity
+        public ProcessResult WaitInvoke<T>(EntityAction<T> action, bool throwOnFail = true)
+            where T : Entity
         {
             InvokeResult invokeResult = Invoke(action);
 
@@ -156,9 +204,11 @@ namespace SOAPLikeWrapperForREST
                 {
                     case ProcessStatus.NotExists:
                     case ProcessStatus.Aborted:
-                        throw new SystemException("Process status: " +
-                                                  processResult.Status + "; Error: " +
-                                                  processResult.Message);
+                        if (throwOnFail)
+                            throw new SystemException("Process status: " +
+                                                      processResult.Status + "; Error: " +
+                                                      processResult.Message);
+                        return processResult;
                     case ProcessStatus.Completed:
                     case ProcessStatus.OK:
                         return processResult;
@@ -182,7 +232,7 @@ namespace SOAPLikeWrapperForREST
 
 
         protected IEnumerable<EntityField> GetSearchFields<T, SearchType>(T entity)
-            where T : Entity
+           where T : Entity
         {
             Type[] searchTypes = new Type[] { typeof(SearchType) };
 
@@ -233,26 +283,89 @@ namespace SOAPLikeWrapperForREST
                 }
             }
         }
-        protected IEnumerable<DetailEntity> GetDetails<T>(T entity)
-                        where T : Entity
+
+        protected IEnumerable<string> GetSubEntitiesWithReturnBehavior(Type entityType, Entity entity, bool returnAll = false)
         {
-            foreach (var field in entity.GetType().GetProperties())
+            List<string> result = new List<string>();
+            foreach (var field in entityType.GetProperties())
             {
                 if (typeof(IEnumerable).IsAssignableFrom(field.PropertyType)
                     && field.Name != "Custom"
                     && field.Name != "Files"
                     && field.PropertyType != typeof(CustomField[])
                     && field.PropertyType != typeof(String)
-                    // && ((IEnumerable)field.GetValue(entity)) != null
                     )
                 {
-                    yield return new DetailEntity(field.PropertyType, ((IEnumerable)field.GetValue(entity)), field.Name);
+                    if (returnAll || (entity != null && entity.ReturnBehavior == ReturnBehavior.All))
+                    {
+                        foreach (var subentity in GetSubEntitiesWithReturnBehavior(field.PropertyType.GetElementType(), null, true))
+                        {
+                            result.Add(field.Name + "/" + subentity);
+                        }
+                        result.Add(field.Name);
+                    }
+                    else
+                    {
+                        Entity item = null;
+                        if (entity != null && field.GetValue(entity) != null)
+                        {
+                            foreach (var detail in (IEnumerable)field.GetValue(entity))
+                            {
+                                if (detail != null)
+                                {
+                                    item = (Entity)detail;
+                                    if (item != null && item.ReturnBehavior == ReturnBehavior.All)
+                                    {
+                                        foreach (var subentity in GetSubEntitiesWithReturnBehavior(field.PropertyType.GetElementType(), null, true))
+                                        {
+                                            result.Add(field.Name + "/" + subentity);
+                                        }
+                                        result.Add(field.Name);
+                                    }
+                                    else if (item != null && item.ReturnBehavior == ReturnBehavior.Default)
+                                    {
+                                        foreach (var subentity in GetSubEntitiesWithReturnBehavior(field.PropertyType.GetElementType(), item))
+                                        {
+                                            result.Add(field.Name + "/" + subentity);
+                                        }
+                                        result.Add(field.Name);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (typeof(Entity).IsAssignableFrom(field.PropertyType))
+                {
+                    Entity item = null;
+                    if (entity != null)
+                    {
+                        item = (Entity)field.GetValue(entity);
+                    }
+                    if (returnAll || (entity != null && entity.ReturnBehavior == ReturnBehavior.All) || (item != null && item.ReturnBehavior == ReturnBehavior.All))
+                    {
+                        foreach (var subentity in GetSubEntitiesWithReturnBehavior(field.PropertyType, null, true))
+                        {
+                            result.Add(field.Name + "/" + subentity);
+                        }
+                        result.Add(field.Name);
+                    }
+                    else if (item != null && item.ReturnBehavior == ReturnBehavior.Default)
+                    {
+                        foreach (var subentity in GetSubEntitiesWithReturnBehavior(field.PropertyType, item))
+                        {
+                            result.Add(field.Name + "/" + subentity);
+                        }
+                        result.Add(field.Name);
+                    }
                 }
             }
+
+            return result.Distinct();
         }
 
         protected IEnumerable<LinkedEntity> GetLinkedEntities<T>(T entity)
-                                    where T : Entity
+            where T : Entity
         {
             foreach (var field in entity.GetType().GetProperties())
             {
@@ -262,76 +375,70 @@ namespace SOAPLikeWrapperForREST
                 }
             }
         }
+
         protected string ComposeFilterForGetList<T>(T entity) where T : Entity
         {
-            string filter = "";
-            foreach (var field in GetSearchFields<T, StringSearch>(entity))
-            {
-                if (field.Value != null)
-                {
-                    filter += field.Name + " eq " + "'" + ((StringSearch)field.Value).Value.ToString() + "' and ";
-                }
-            }
-            foreach (var field in GetSearchFields<T, IntSearch>(entity))
-            {
-                if (field.Value != null)
-                {
-                    filter += field.Name + " eq " + ((IntSearch)field.Value).Value.ToString() + " and ";
-                }
-            }
-            if (!string.IsNullOrEmpty(filter))
-                filter = filter.Substring(0, filter.Length - 4);
-            return filter;
+            List<string> filters = new List<string>();
+
+            filters.AddRange(GetSearchFields<T, StringSearch>(entity)
+                .Select(GetFilterByStringCond));
+
+            filters.AddRange(GetSearchFields<T, IntSearch>(entity)
+                .Select(field => field.Name + " eq " + ((IntSearch)field.Value).Value.ToString()));
+
+            filters.AddRange(GetSearchFields<T, BooleanSearch>(entity)
+                .Select(field => field.Name + " eq " + ((BooleanSearch)field.Value).Value.ToString().ToLower()));
+
+            filters.AddRange(GetSearchFields<T, DateTimeSearch>(entity)
+                .Select(GetFilterByDateTimeCond));
+
+            return string.Join(" and ", filters);
         }
+
+        private string GetFilterByStringCond(EntityField field)
+        {
+            StringSearch search = (StringSearch)field.Value;
+
+            switch (search.Condition)
+            {
+                case StringCondition.IsNotNull: return field.Name + " ne null";
+                case StringCondition.Contains: return "substringof('" + search.Value.ToString() + "', " + field.Name + ")";
+                case StringCondition.StartsWith: return "startswith(" + field.Name + ", '" + search.Value.ToString() + "')";
+                case StringCondition.NotEqual: return field.Name + " ne '" + search.Value.ToString() + "'";
+                case StringCondition.DoesNotContain: return "substringof('" + search.Value.ToString() + "', " + field.Name + ") eq false";
+                case StringCondition.Equal: return field.Name + " eq '" + search.Value.ToString() + "'";
+                default: throw new NotImplementedException($"Condition {search.Condition} is not implemented");
+            }
+        }
+
+        private static string GetFilterByDateTimeCond(EntityField field)
+        {
+            DateTimeSearch search = (DateTimeSearch)field.Value;
+
+            switch (search.Condition)
+            {
+                case DateTimeCondition.Equal: return field.Name + " eq datetimeoffset'" + search.Value?.ToString("o") + "'";
+                case DateTimeCondition.IsGreaterThan: return field.Name + " gt datetimeoffset'" + search.Value?.ToString("o") + "'";
+                default: throw new NotImplementedException($"Condition {search.Condition} is not implemented");
+            }
+        }
+
         protected string ComposeFilter<T>(T entity) where T : Entity
         {
-            string filter = "";
-            foreach (var field in GetPossibleKeyFields(entity))
-            {
-                if (field.Value != null)
+            return string.Join(" and ", GetPossibleKeyFields(entity)
+                .Where(f => f.Value != null)
+                .Select(field =>
                 {
                     if (field.Type == typeof(StringValue))
-                        filter += field.Name + " eq " + "'" + field.Value.ToString() + "' and ";
+                        return field.Name + " eq " + "'" + field.Value.ToString() + "'";
                     else
-                        filter += field.Name + " eq " + field.Value.ToString() + " and ";
-                }
-            }
-
-            filter = filter.Substring(0, filter.Length - 4);
-            return filter;
+                        return field.Name + " eq " + field.Value.ToString();
+                }));
         }
+
         protected string ComposeExpands<T>(T entity) where T : Entity
         {
-            string expand = "";
-            foreach (DetailEntity detailEntity in GetDetails(entity))
-            {
-                if (entity.ReturnBehavior == ReturnBehavior.All)
-                {
-                    expand += detailEntity.Name += ",";
-                }
-                else if (detailEntity.Details != null)
-                {
-                    foreach (var row in detailEntity.Details)
-                    {
-                        if (row?.ReturnBehavior == ReturnBehavior.All || row?.ReturnBehavior == ReturnBehavior.Default)
-                        {
-                            expand += detailEntity.Name += ",";
-                            break;
-                        }
-                    }
-                }
-            }
-            foreach (LinkedEntity linkedEntity in GetLinkedEntities(entity))
-            {
-                if (entity.ReturnBehavior == ReturnBehavior.All || linkedEntity?.Value?.ReturnBehavior == ReturnBehavior.All || linkedEntity?.Value?.ReturnBehavior == ReturnBehavior.Default)
-                {
-                    expand += linkedEntity.Name += ",";
-                }
-            }
-
-            expand = expand.TrimEnd(',');
-
-            return expand;
+            return string.Join(",", GetSubEntitiesWithReturnBehavior(entity.GetType(), entity));
         }
         protected int GetProcessingSeconds(string invokeResult)
         {
