c96348f95c45e9f69cd53ddf271fb5b6998c9a20
alaraalara <alaraozkutucu@gmail.com>
2022-09-13 17:29:08 -0400

    OData Api derives from BaseApi. In that way, the repetition of the code has been prevented. It uses the code from Configuration.cs and BaseApi.cs

--- Acumatica REST API Console Application/Program.cs
+++ Acumatica REST API Console Application/Program.cs
@@ -16,7 +16,7 @@ namespace AcumaticaRestApiExample
 	class Program
 	{
 		const string SiteURL = "http://localhost/demo/";
-		const string Username = "admin";
+        const string Username = "admin";
 		const string Password = "123";
 		const string Tenant = null;
 		const string Branch = null;
--- Acumatica.RESTClient/BaseApi/ODataAPI.cs
+++ Acumatica.RESTClient/BaseApi/ODataAPI.cs
@@ -12,136 +12,68 @@ using RestSharp.Authenticators;
 namespace Acumatica.RESTClient.Api
 {
 
-    public class ODataAPI
+    public class ODataAPI : BaseApi
     {
-        protected readonly string Username;
-        protected readonly string Password;
-        protected string BasePath;
+      
         protected ODataVersion Version;
         protected string Tenant;
         protected Token Token;
         
-        //for oauth initialization
-        public ODataAPI(Configuration configuration, ODataVersion version, string tenant=null) 
+        public ODataAPI(Configuration configuration, ODataVersion version, string tenant=null): base(configuration)
         {
-            Username = configuration.Username;
-            Password = configuration.Password;
-            BasePath = configuration.BasePath;
             Version = version;
             Tenant = tenant;
             Token = configuration.Token;
-            
-
-        }
-        
-        //for basic authentication
-        public ODataAPI(string username, string password, string basePath, ODataVersion version, string tenant = null)
-        {
-            Username = username;
-            Password = password;
-            BasePath = basePath;
-            Version = version;
-            Tenant = tenant;
         }
       
-        public string Get(string resource = null, string select = null, string filter = null, string expand = null, string custom = null, int? skip = null, int? top = null)
+       
+        public ApiResponse<string> Get(string resource = null, string select = null, string filter = null, string expand = null, string custom = null, int? skip = null, int? top = null)
         {
-            RestRequest request;
-            RestResponse response;
-            var path = ConfigurePath(Version.ToString(), Tenant);
-            RestClient client = new RestClient(path);
-            //Oauth authentication
-            if (Token != null)
-            {
-                request = GetOauthAuthentication(client);
-            }   
+            var path = ConfigureResourcePath(ConfigurePath(), resource);
 
             //Basic authentication
-            else
+            if (Token == null)
             {
-                request = GetBasicAuthentication(client, resource);                
+                BasicAuthentication(this.Configuration.ApiClient.RestClient);
             }
-            AddParameters(request, select, filter, expand, custom, skip, top);
-            response = client.ExecuteAsync(request).Result;
-
-            ApiResponse<string> apiresponse = new ApiResponse<string>((int)response.StatusCode, response.Headers
-                            .Where(x => x.Name != "Set-Cookie")
-                            .ToDictionary(x => x.Name, x => x.Value.ToString()),
-                            response.Content);
-
-            return apiresponse.Data;
+            RestResponse response = (RestResponse)Configuration.ApiClient.CallApiAsync(path, Method.Get, ComposeQueryParams(select, filter, expand, custom, skip, top), null, ComposeAcceptHeaders(HeaderContentType.Json), ComposeEmptyFormParams(), ComposeEmptyFileParams(), ComposeEmptyPathParams(), ComposeContentHeaders(HeaderContentType.Json)).Result;         
+            return DeserializeResponse<string>(response);
         }
-
-
 
 
         #region Implementation
 
-
         /// <summary>
-        /// Composes Query Parameters for API Request. 
-        /// </summary>
-        /// <param name="select">The fields of the entity to be returned from the system. (optional)</param>
-        /// <param name="filter">The conditions that determine which records should be selected from the system. (optional)</param>
-        /// <param name="expand">The linked and detail entities that should be expanded. (optional)</param>
-        /// <param name="custom">The fields that are not defined in the contract of the endpoint to be returned from the system. (optional)</param>
-        /// <param name="skip">The number of records to be skipped from the list of returned records. (optional)</param>
-        /// <param name="top">The number of records to be returned from the system. (optional)</param>
-        protected void AddParameters(RestRequest request, string select = null, string filter = null, string expand = null, string custom = null, int? skip = null, int? top = null)
-        {
-
-            var queryParameters = new Dictionary<string, string>();
-            queryParameters.Add("$format", "json");
-            if (!String.IsNullOrEmpty(select)) queryParameters.Add("$select", select); // query parameter
-            if (!String.IsNullOrEmpty(filter)) queryParameters.Add("$filter", filter); // query parameter
-            if (!String.IsNullOrEmpty(expand)) queryParameters.Add("$expand", expand); // query parameter
-            if (!String.IsNullOrEmpty(custom)) queryParameters.Add("$custom", custom); // query parameter
-            if (skip != null) queryParameters.Add("$skip", skip.ToString()); // query parameter
-            if (top != null) queryParameters.Add("$top", top.ToString()); // query parameter
-            foreach (var parameter in queryParameters)
-            {
-                request.AddParameter(parameter.Key, parameter.Value);
-            }
-
-        }
-
-        private string ConfigurePath(string version, string tenant = null)
+		/// Configures the base path according to version of OData and tenant, if exists.
+		/// </summary>
+        private string ConfigurePath()
         {
             string path;
-            if (tenant == null)
+            if (Tenant == null)
             {
-                path = BasePath + "/" + version;
+                path = this.Configuration.BasePath + "/" + Version;
             }
             else
             {
-                path = BasePath + "/" + version + "/" + tenant;
+                path = GetBasePath() + "/" + Version + "/" + Tenant;
             }
             return path;
         }
 
-        private RestRequest GetOauthAuthentication(RestClient client)
+        private string ConfigureResourcePath(string path, string resource = null)
         {
-            var request = new RestRequest();
-            client.Authenticator = new JwtAuthenticator(Token.Access_token);
-            return request;
-        }
-
-        private RestRequest GetBasicAuthentication(RestClient client, string resource = null)
-        {
-            client.Authenticator = new HttpBasicAuthenticator(Username, Password);
-            RestRequest request;
             if (resource != null)
             {
-                request = new RestRequest(resource);
+                path = path + "/" + resource;
             }
-            else
-            {
-                request = new RestRequest();
-            }
-            return request;
-
-
+            return path;
         }
+ 
+        private void BasicAuthentication(RestClient client)
+        {
+            client.Authenticator = new HttpBasicAuthenticator(this.Configuration.Username, this.Configuration.Password);
+        }
+        
 
         #endregion
     }
