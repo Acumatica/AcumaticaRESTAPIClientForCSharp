f966693921795d3195319970ae7a0517763e7c1b
dnaumov <dnaumov@acumatica.com>
2023-02-15 16:13:12 -0500

    Update Odata example to be more useful

--- Acumatica REST API Console Application/ODataExample.cs
+++ Acumatica REST API Console Application/ODataExample.cs
@@ -17,65 +17,56 @@ namespace AcumaticaRestApiExample
 				requestInterceptor: RequestLogger.LogRequest, responseInterceptor: RequestLogger.LogResponse);
 			var configuration = authApi.ReceiveAccessToken(clientID, clientSecret, username, password, OAuthScope.API);
 			
-			ODataAPI ODataOauthExample = new ODataAPI(configuration, ODataVersion.OData);
-			var response = ODataOauthExample.Get();
-			Console.WriteLine(response);
+			ODataAPI ODataOauthExample = new ODataAPI(configuration, ODataVersion.OData, tenant);
+			var response = ODataOauthExample.GetMetadata();
+			Console.WriteLine(response.Data);
 		}
-		
-		public static void ODataGetV3(string siteURL, string username, string password, string tenant=null, string branch=null, string locale=null)
-		{						
-                Console.WriteLine("OData version 3 with Basic Authentication");
-                var conf = new Configuration(siteURL, requestInterceptor: RequestLogger.LogRequest, responseInterceptor: RequestLogger.LogResponse);
-				conf.Username= username;
-				conf.Password = password;
-				ODataAPI odatav3 = new ODataAPI(conf, ODataVersion.OData);
-				var response = odatav3.Get();
-				Console.WriteLine(response.Data);
 
-				Console.WriteLine("Example retrieval of customers with OData Version 3");
-				var response2 = odatav3.Get("Customer%20Contacts");
-				Console.WriteLine(response2);
+		public static void ODataGetV3(string siteURL, string username, string password, string tenant = null, string branch = null, string locale = null)
+		{
+			Console.WriteLine("OData version 3 with Basic Authentication");
+			var conf = new Configuration(siteURL, requestInterceptor: RequestLogger.LogRequest, responseInterceptor: RequestLogger.LogResponse);
+			conf.Username = username;
+			conf.Password = password;
+			ODataAPI odatav3 = new ODataAPI(conf, ODataVersion.OData, tenant);
+			var response = odatav3.GetMetadata();
+			Console.WriteLine(response.Data);
 
-				Console.WriteLine("Retrieving quantities from tables with OData Version 3");
-				var response3 = odatav3.Get("Item%20Availability%20Data");
-				Console.WriteLine(response3);
 
-				Console.WriteLine("Filtering the Result of a Generic Inquiry with OData Version 3");
-				var response4 = odatav3.Get("$metadata");
-				Console.WriteLine(response4);
+			Console.WriteLine("Filtering the Result of a Generic Inquiry with OData Version 3");
+			response = odatav3.GetOData("BI-ARInvoices");
 
-				Console.WriteLine("Retrieve the modified stocks with OData Version 3");
-				string filterParam = "ItemStatus eq 'Active' and LastModifiedOn gt datetime'2022-07-13T00:00:00.000'";
-				var response5 = odatav3.Get("Modified%20Stock%20Items", null, filterParam);
-				Console.WriteLine(response5);
-				
-			
-			
-        }
+			Console.WriteLine(response.Data);
+
+			Console.WriteLine("Retrieve the modified stocks qtys with OData Version 3");
+			string filterParam = "LastModifiedDateTime gt datetime'2022-07-13T00:00:00.000'";
+			response = odatav3.GetOData("DB-StorageDetails", filter: filterParam);
+			Console.WriteLine(response.Data);
+		}
         
-		public static void ODataGetV4(string siteURL, string username, string password)
+		public static void ODataGetV4(string siteURL, string username, string password, string tenant)
         {
 			Console.WriteLine("OData version 4 examples");
 			Console.WriteLine("Testing sign in");
             var conf = new Configuration(siteURL, requestInterceptor: RequestLogger.LogRequest, responseInterceptor: RequestLogger.LogResponse);
             conf.Username = username;
             conf.Password = password;
-            ODataAPI oDatav4 = new ODataAPI(conf, ODataVersion.ODatav4);
-			var response = oDatav4.Get("$metadata");
-			Console.WriteLine(response);
+            ODataAPI oDatav4 = new ODataAPI(conf, ODataVersion.ODatav4, tenant);
+			var response = oDatav4.GetMetadata();
+			Console.WriteLine(response.Data);
 
 			Console.WriteLine("Retrieving quantities from tables with OData Version 4");
 			string selectParam = "QtyOnHand,QtyAvail";
 			string expandParam = "InventoryItemByInventoryID($select=InventoryCD,Descr),INSiteBySiteID($select = SiteCD)";
-			var response3 = oDatav4.Get("PX_Objects_IN_INSiteStatus", selectParam, null, expandParam);
-			Console.WriteLine(response3);
+			var response3 = oDatav4.GetOData("PX_Objects_IN_INSiteStatus", selectParam, null, expandParam);
+			Console.WriteLine(response3.Data);
 
 			Console.WriteLine("Retrieve the list of modified stocks with OData Version 4");
 			selectParam = "InventoryCD,Descr,ItemStatus,LastModifiedDateTime,BaseUnit";
 			expandParam = "INSiteByDfltSiteID($select=SiteCD),INItemClassByItemClassID($select = ItemClassCD),INSiteStatusCollection($select = QtyOnHand)";
 			string filterParam = "StkItem eq true and ItemStatus eq 'AC' and LastModifiedDateTime gt 2022-07-13T00:00-04:00";
-			var response5 = oDatav4.Get("PX_Objects_IN_InventoryItem", selectParam, filterParam, expandParam);
-			Console.WriteLine(response5);
+			var response5 = oDatav4.GetOData("PX_Objects_IN_InventoryItem", selectParam, filterParam, expandParam);
+			Console.WriteLine(response5.Data);
 
         }
 		
