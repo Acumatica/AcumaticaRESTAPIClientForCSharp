3377a3226b5a0f69009c4a32ea03447083e5d0fa
dnaumov <dnaumov@acumatica.com>
2023-05-12 17:10:22 -0400

    Add try catch to logger to not fail the main flow if logging fails

--- Acumatica REST API Console Application/RequestLogger.cs
+++ Acumatica REST API Console Application/RequestLogger.cs
@@ -14,15 +14,21 @@ namespace AcumaticaRestApiExample
 		/// </summary>
 		public static void LogResponse(HttpResponseMessage responseMessage)
 		{
-			StreamWriter writer = new StreamWriter(RequestsLogPath, true);
-			writer.WriteLine(DateTime.Now.ToString());
-			writer.WriteLine("Response");
-			writer.WriteLine("\tStatus code: " + responseMessage.StatusCode);
-			writer.WriteLine("\tContent: " + responseMessage?.Content.ReadAsStringAsync().Result);
-			writer.WriteLine("-----------------------------------------");
-			writer.WriteLine();
-			writer.Flush();
-			writer.Close();
+			try
+			{
+				using (var writer = new StreamWriter(RequestsLogPath, true))
+				{
+					writer.WriteLine(DateTime.Now.ToString());
+					writer.WriteLine("Response");
+					writer.WriteLine("\tStatus code: " + responseMessage.StatusCode);
+					writer.WriteLine("\tContent: " + responseMessage?.Content.ReadAsStringAsync().Result);
+					writer.WriteLine("-----------------------------------------");
+					writer.WriteLine();
+					writer.Flush();
+					writer.Close();
+				}
+			}
+			catch { }
 		}
 
 		/// <summary>
@@ -30,19 +36,26 @@ namespace AcumaticaRestApiExample
 		/// </summary>
 		public static void LogRequest(HttpRequestMessage request)
 		{
-			StreamWriter writer = new StreamWriter(RequestsLogPath, true);
-			writer.WriteLine(DateTime.Now.ToString());
-			writer.WriteLine("Request");
-			writer.WriteLine("\tMethod: " + request.Method);
-			string body = request.Content?.ReadAsStringAsync().Result;
+            try
+			{
+				using (var writer = new StreamWriter(RequestsLogPath, true))
+				{
+					writer.WriteLine(DateTime.Now.ToString());
+					writer.WriteLine("Request");
+					writer.WriteLine("\tMethod: " + request.Method);
+					string body = request.Content?.ReadAsStringAsync().Result;
 
-			writer.WriteLine("\tURL: " + request.RequestUri);
-			if (!String.IsNullOrEmpty(body))
-				writer.WriteLine("\tBody: " + body);
-			writer.WriteLine("-----------------------------------------");
-			writer.WriteLine();
-			writer.Flush();
-			writer.Close();
+					writer.WriteLine("\tURL: " + request.RequestUri);
+					if (!String.IsNullOrEmpty(body))
+						writer.WriteLine("\tBody: " + body);
+					writer.WriteLine("-----------------------------------------");
+					writer.WriteLine();
+					writer.Flush();
+					writer.Close();
+				}
+			}
+			catch { }
+
 		}
 	}
 }
