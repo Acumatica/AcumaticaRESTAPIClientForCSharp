c4e2ef1cf15c749c6b0de45f795266e09ad66676
Dmitrii Naumov <dnaumov@acumatica.com>
2024-09-26 12:17:04 -0400

    Add DACBrowserApi

---
+++ Acumatica.RESTClient.DACBrowserApi/Model/Documentation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Acumatica.RESTClient.DACBrowserApi.Model
+{
+    [DataContract]
+    public class Documentation
+    {
+        [DataMember(Name = "summary", EmitDefaultValue = false)]
+        public string Summary { get; set; }
+
+        [DataMember(Name = "remarks", EmitDefaultValue = false)]
+        public string Remarks { get; set; }
+    }
+}
---
+++ Acumatica.RESTClient.DACBrowserApi/Model/Field.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Acumatica.RESTClient.DACBrowserApi.Model
+{
+    [DataContract]
+    public class Field
+    {
+        [DataMember(Name = "name", EmitDefaultValue = false)]
+        public string Name { get; set; }
+
+        [DataMember(Name = "isKey", EmitDefaultValue = false)]
+        public bool IsKey { get; set; }
+
+        [DataMember(Name = "dacName", EmitDefaultValue = false)]
+        public string DacName { get; set; }
+
+        [DataMember(Name = "displayName", EmitDefaultValue = false)]
+        public string DisplayName { get; set; }
+
+        [DataMember(Name = "sqlType", EmitDefaultValue = false)]
+        public string SqlType { get; set; }
+
+        [DataMember(Name = "documentation", EmitDefaultValue = false)]
+        public Documentation Documentation { get; set; }
+
+    }
+}
