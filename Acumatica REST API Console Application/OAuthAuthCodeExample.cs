a6d9f5f4780f35dfabe7923d46339de08a905c30
Dmitrii Naumov <dnaumov@acumatica.com>
2024-10-08 17:16:30 -0400

    Fix OAuth authorization code flow in the example

--- Acumatica REST API Console Application/OAuthAuthCodeExample.cs
+++ Acumatica REST API Console Application/OAuthAuthCodeExample.cs
@@ -83,7 +83,6 @@ namespace AcumaticaRestApiExample
                 // hack because of this: https://github.com/dotnet/corefx/issues/10361
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    url = url.Replace("&", "^&");
                     Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
