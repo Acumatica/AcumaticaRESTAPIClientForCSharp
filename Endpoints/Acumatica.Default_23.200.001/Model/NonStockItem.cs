3d31ee88a920c2d40e5e9c46d93dadec16a5d9a8
Dmitrii Naumov <dnaumov@acumatica.com>
2025-12-08 11:06:14 -0500

    Update endpoint descriptions

--- Endpoints/Acumatica.Default_23.200.001/Model/NonStockItem.cs
+++ Endpoints/Acumatica.Default_23.200.001/Model/NonStockItem.cs
@@ -386,6 +386,7 @@ namespace Acumatica.Default_23_200_001.Model
 		public DecimalValue? CurySpecificPrice { get; set; }
 
 		/// <summary>
+		/// Indicates the availability setting for the inventory item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
 		/// <para>SQL Type: char(1)</para>
 		/// </summary>
@@ -421,7 +422,10 @@ namespace Acumatica.Default_23_200_001.Model
 		public DecimalValue? CurrentStdCost { get; set; }
 
 		/// <summary>
+		/// The URL to use for this inventory item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Custom URL</para>
+		/// <para>SQL Type: nvarchar(100)</para>
 		/// </summary>
 		[DataMember(Name="CustomURL", EmitDefaultValue=false)]
 		public StringValue? CustomURL { get; set; }
@@ -439,13 +443,19 @@ namespace Acumatica.Default_23_200_001.Model
 		public List<InventoryFileUrls>? FileUrls { get; set; }
 
 		/// <summary>
+		/// The meta description for this item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Meta Description</para>
+		/// <para>SQL Type: nvarchar(1024)</para>
 		/// </summary>
 		[DataMember(Name="MetaDescription", EmitDefaultValue=false)]
 		public StringValue? MetaDescription { get; set; }
 
 		/// <summary>
+		/// A comma-separated list of meta keywords for this item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Meta Keywords</para>
+		/// <para>SQL Type: nvarchar(1024)</para>
 		/// </summary>
 		[DataMember(Name="MetaKeywords", EmitDefaultValue=false)]
 		public StringValue? MetaKeywords { get; set; }
@@ -462,13 +472,19 @@ namespace Acumatica.Default_23_200_001.Model
 		public GuidValue? NoteID { get; set; }
 
 		/// <summary>
+		/// The title to use on this item's page.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Page Title</para>
+		/// <para>SQL Type: nvarchar(100)</para>
 		/// </summary>
 		[DataMember(Name="PageTitle", EmitDefaultValue=false)]
 		public StringValue? PageTitle { get; set; }
 
 		/// <summary>
+		/// A comma-separated list of search keywords for this item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Search Keywords</para>
+		/// <para>SQL Type: nvarchar(1024)</para>
 		/// </summary>
 		[DataMember(Name="SearchKeywords", EmitDefaultValue=false)]
 		public StringValue? SearchKeywords { get; set; }
@@ -482,6 +498,7 @@ namespace Acumatica.Default_23_200_001.Model
 		public StringValue? TemplateItemID { get; set; }
 
 		/// <summary>
+		/// Indicates the visibility for this inventory item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
 		/// <para>SQL Type: char(1)</para>
 		/// </summary>
--- Endpoints/Acumatica.Default_23.200.001/Model/StockItem.cs
+++ Endpoints/Acumatica.Default_23.200.001/Model/StockItem.cs
@@ -656,6 +656,7 @@ namespace Acumatica.Default_23_200_001.Model
 		public DecimalValue? CurySpecificPrice { get; set; }
 
 		/// <summary>
+		/// Indicates the availability setting for the inventory item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
 		/// <para>SQL Type: char(1)</para>
 		/// </summary>
@@ -663,7 +664,10 @@ namespace Acumatica.Default_23_200_001.Model
 		public StringValue? Availability { get; set; }
 
 		/// <summary>
+		/// The URL to use for this inventory item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Custom URL</para>
+		/// <para>SQL Type: nvarchar(100)</para>
 		/// </summary>
 		[DataMember(Name="CustomURL", EmitDefaultValue=false)]
 		public StringValue? CustomURL { get; set; }
@@ -679,13 +683,19 @@ namespace Acumatica.Default_23_200_001.Model
 		public List<InventoryFileUrls>? FileURLs { get; set; }
 
 		/// <summary>
+		/// The meta description for this item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Meta Description</para>
+		/// <para>SQL Type: nvarchar(1024)</para>
 		/// </summary>
 		[DataMember(Name="MetaDescription", EmitDefaultValue=false)]
 		public StringValue? MetaDescription { get; set; }
 
 		/// <summary>
+		/// A comma-separated list of meta keywords for this item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Meta Keywords</para>
+		/// <para>SQL Type: nvarchar(1024)</para>
 		/// </summary>
 		[DataMember(Name="MetaKeywords", EmitDefaultValue=false)]
 		public StringValue? MetaKeywords { get; set; }
@@ -694,13 +704,19 @@ namespace Acumatica.Default_23_200_001.Model
 		public GuidValue? NoteID { get; set; }
 
 		/// <summary>
+		/// The title to use on this item's page.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Page Title</para>
+		/// <para>SQL Type: nvarchar(100)</para>
 		/// </summary>
 		[DataMember(Name="PageTitle", EmitDefaultValue=false)]
 		public StringValue? PageTitle { get; set; }
 
 		/// <summary>
+		/// A comma-separated list of search keywords for this item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
+		/// <para>Display Name: Search Keywords</para>
+		/// <para>SQL Type: nvarchar(1024)</para>
 		/// </summary>
 		[DataMember(Name="SearchKeywords", EmitDefaultValue=false)]
 		public StringValue? SearchKeywords { get; set; }
@@ -714,6 +730,7 @@ namespace Acumatica.Default_23_200_001.Model
 		public StringValue? TemplateItemID { get; set; }
 
 		/// <summary>
+		/// Indicates the visibility for this inventory item.
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
 		/// <para>SQL Type: char(1)</para>
 		/// </summary>
@@ -721,6 +738,7 @@ namespace Acumatica.Default_23_200_001.Model
 		public StringValue? Visibility { get; set; }
 
 		/// <summary>
+		/// Indicates the Not Available Setting to use when an item has no more quantity available.
 		/// <para>DAC Field Name: NotAvailMode</para>
 		/// <para>DAC: PX.Objects.IN.InventoryItem</para>
 		/// <para>Display Name: When Qty Unavailable</para>
