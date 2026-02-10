7199419e479a4abeda5e8a22bf625c027795a2b1
Evgeny Afanasiev <evgeny.afanasiev@acumatica.com>
2026-02-10 17:01:14 +0300

    Reverting original line breaks

--- Acumatica.RESTClient/AuthApi/AuthApi.cs
+++ Acumatica.RESTClient/AuthApi/AuthApi.cs
@@ -31,7 +31,7 @@ namespace Acumatica.RESTClient.AuthApi
         {
             if (client == null || string.IsNullOrEmpty(client.Token?.Refresh_token))
                 ThrowMissingParameter(nameof(RefreshAccessToken), "Refresh_Token");
-			var time = DateTime.UtcNow;
+            var time = DateTime.UtcNow;
             HttpResponseMessage response = await client!.CallApiAsync(
                 resourcePath:       "/identity/connect/token",
                 method:             HttpMethod.Post,
@@ -51,7 +51,7 @@ namespace Acumatica.RESTClient.AuthApi
             response.EnsureSuccessStatusCode();
 
             client.Token = await DeserializeAsync<Token>(response).ConfigureAwait(false);
-            client.Token?.SetTokenObtainedDT(time);							   
+            client.Token?.SetTokenObtainedDT(time);
         }
         /// <summary>
         /// Receives access token for OAuth 2.0 authentication (Resource owner password credentials flow)
@@ -85,7 +85,7 @@ namespace Acumatica.RESTClient.AuthApi
             OAuthScope scope, 
             CancellationToken cancellationToken = default)
         {
-            var time = DateTime.UtcNow;						   
+            var time = DateTime.UtcNow;
             HttpResponseMessage response = await client.CallApiAsync(
                resourcePath:        "identity/connect/token",
                method:              HttpMethod.Post,
@@ -106,7 +106,7 @@ namespace Acumatica.RESTClient.AuthApi
             await VerifyResponseAsync(client, response, nameof(ReceiveAccessTokenAsync)).ConfigureAwait(false);
 
             client.Token = await DeserializeAsync<Token>(response).ConfigureAwait(false);
-			client.Token?.SetTokenObtainedDT(time);									   
+            client.Token?.SetTokenObtainedDT(time);
         }
 
         /// <summary>
@@ -196,7 +196,7 @@ namespace Acumatica.RESTClient.AuthApi
             string code, 
             CancellationToken cancellationToken = default)
         {
-			var time = DateTime.UtcNow;						   
+            var time = DateTime.UtcNow;
             HttpResponseMessage response = await client.CallApiAsync(
                resourcePath:        "/identity/connect/token",
                method:              HttpMethod.Post,
@@ -217,7 +217,7 @@ namespace Acumatica.RESTClient.AuthApi
             await VerifyResponseAsync(client, response, "RequestToken").ConfigureAwait(false);
 
             client.Token = await DeserializeAsync<Token>(response).ConfigureAwait(false);
-			client.Token?.SetTokenObtainedDT(time);									   
+            client.Token?.SetTokenObtainedDT(time);
         }
         #endregion
 
